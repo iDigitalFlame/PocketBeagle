c172a73e6277b8534060316f686ffbb7b60c535a
iDigitalFlame <idf@idigitalflame.com>
2021-12-12 16:56:37 -0500

    Fixes for licenses

--- build-config.py
+++ build-config.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
+# Copyright 2021 - 2022 iDigitalFlame
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 from os import listdir
 from io import StringIO
@@ -113,7 +127,7 @@ class Builder(object):
         if isfile(path):
             return self._file(path)
         if not isdir(path):
-            print(f'Ignoring non file/dir entry "{path}".', file=stderr)
+            print(f'Ignoring non-file/dir entry "{path}".', file=stderr)
             return
         for f in listdir(path):
             p = join(path, f)
