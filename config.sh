00b4ef870435536ab78b4a1dad4e431a6882a415
iDigitalFlame <idf@idigitalflame.com>
2023-01-03 17:42:34 -0500

    Updated umask

--- config.sh
+++ config.sh
@@ -195,7 +195,7 @@ mkdir -p "${ROOT}${SYSCONFIG_DIR}/etc" 2> /dev/null
 
 # Create file "/etc/profile.d/umask.sh"
 /usr/bin/printf "" > "${ROOT}${SYSCONFIG_DIR}/etc/profile.d/umask.sh"
-/usr/bin/printf '#!/usr/bin/bash\n\numask 027\n' >> "${ROOT}${SYSCONFIG_DIR}/etc/profile.d/umask.sh"
+/usr/bin/printf '#!/usr/bin/bash\n\numask 0027\n' >> "${ROOT}${SYSCONFIG_DIR}/etc/profile.d/umask.sh"
 
 # Create file "/etc/profile.d/z_system_status.sh"
 /usr/bin/printf "" > "${ROOT}${SYSCONFIG_DIR}/etc/profile.d/z_system_status.sh"
